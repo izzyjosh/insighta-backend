eb5263169c5c2ff77d125471d1cf8b419b0f37c7
Joseph Joshua <calebayuba222@gmail.com>
2026-04-28 09:40:52 +0100

    feat(models): add User and RefreshToken models

---
+++ src/models/User.model.ts
@@ -0,0 +1,42 @@
+import { BeforeInsert, Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { uuidv7 } from 'uuidv7';
+
+export enum UserRole {
+  ANALYST = 'analyst',
+  ADMIN = 'admin',
+}
+
+@Entity()
+export class User {
+  @PrimaryColumn("uuid")
+  id!: string;
+
+  @Column({ type: 'varchar', unique: true })
+  github_id!: string;
+
+  @Column({ type: 'varchar' })
+  username!: string;
+
+  @Column({ type: 'varchar' })
+  email!: string;
+
+  @Column({ type: 'varchar' })
+  avatar_url!: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.ANALYST })
+  role!: UserRole;
+
+  @Column({ type: 'boolean', default: false })
+  is_active!: boolean;
+
+  @Column({type: 'timestamp'})
+  last_login!: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at!: Date;
+
+  @BeforeInsert()
+    generateId() {
+    this.id = uuidv7();
+  }
+}
